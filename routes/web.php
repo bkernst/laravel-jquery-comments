885b94da19d492751fccbe965c183307e3d44a31
Bernhard Ernst <bkernst@gmail.com>
2025-05-10 16:13:42 +0200

    Added changed route

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,14 @@ Route::get('/cms', [CommentController::class, 'cms'])->name('cms');
 Route::prefix('api')->name('api.')->group(function () {
     Route::prefix('cms')->name('cms.')->group(function () {
         Route::get('/all', [CommentController::class, 'all_cms'])->name('all_cms');
+        Route::get('/changed', [CommentController::class, 'changed_cms'])->name('changed_cms');
         Route::delete('/delete', [CommentController::class, 'delete'])->name('delete');
         // Route::patch('/approve', [CommentController::class, 'approve'])->name('approve');
         // Route::patch('/reject', [CommentController::class, 'reject'])->name('reject');
     });
     Route::prefix('frontend')->name('frontend.')->group(function () {
         Route::get('/all', [CommentController::class, 'all'])->name('all');
+        Route::get('/changed', [CommentController::class, 'changed'])->name('changed');
         Route::post('/reply', [CommentController::class, 'reply'])->name('reply');
     });
 });
