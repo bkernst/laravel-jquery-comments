f306639c0c2c4c00641beb877770c38345a37710
Bernhard Ernst <bkernst@gmail.com>
2025-05-10 16:13:15 +0200

    Added clear filter

--- public/js/shared.js
+++ public/js/shared.js
@@ -1,3 +1,6 @@
+let commentIdsData = [];
+let commentIdsExisting = [];
+
 function validateEmail(email) {
     return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 }
@@ -7,7 +10,7 @@ function closePopup() {
     $('.popup').removeClass('open');
 }
 
-function renderComments(data) {
+function renderComments(data, clear) {
     if (data === null) {
         console.error('Data is null');
         return false;
@@ -16,37 +19,39 @@ function renderComments(data) {
         console.error('Data is not an object');
         return false;
     }
-    if (data.length === 0) {
+    if (clear && data.length === 0) {
         $('#comments').empty();
         $('#comments').html('<li class="no-comments-found pb-1 pt-1"><p class="mt-1 mb-1 pb-0 text-center">No comments found</p></li>');
         return false;
     }
     $('li.no-comments-found').remove();
 
-    // Get comment IDs to check which comments need to be removed later
-    let commentIdsExisting = [];
-    $('li').each(function() {
-        commentIdsExisting.push($(this).data('id'));
-    });
+    if (clear) {
+        // Get comment IDs to check which comments need to be removed later
+        commentIdsExisting = [];
+        $('li').each(function() {
+            commentIdsExisting.push($(this).data('id'));
+        });
 
-    // Render comments
-    let commentIdsData = [];
-    for (let index in data) {
-        renderComment(data[index]);
-        commentIdsData.push(data[index].id);
+        // Render comments
+        commentIdsData = [];
+        for (const index in data) {
+            renderComment(data[index]);
+            commentIdsData.push(data[index].id);
+        }
     }
 
     // Remove existing comments not found in the new list
-    let removedIds = commentIdsExisting.filter(item => !commentIdsData.includes(item));
-    if (removedIds.length > 0) {
-        for (let index in removedIds) {
-            $('li[data-id="' + removedIds[index] + '"]').remove();
-        }
-    }
+    // const removedIds = commentIdsExisting.filter(item => !commentIdsData.includes(item));
+    // if (removedIds.length > 0) {
+    //     for (const index in removedIds) {
+    //         $('li[data-id="' + removedIds[index] + '"]').remove();
+    //     }
+    // }
 }
 
 function setBarDisplay(message, className) {
-    let selector = '.' + className + '-bar';
+    const selector = '.' + className + '-bar';
     if (message !== '') {
         $(selector + ' > .center-holder').text(message);
         $(selector).addClass('open');
